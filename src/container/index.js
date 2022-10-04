6372982f435df30782d225b83cf03393edf487cf
princexXXx10 <victorsuccess6@gmail.com>
2022-10-04 22:22:46 +0100

    A

--- src/container/index.js
+++ src/container/index.js
@@ -1,19 +1,13 @@
-import AboutUs from './AboutUs/AboutUs';
-import Chef from './Chef/Chef';
 import Newsletter from './Newsletter/Newsletter';
 import Footer from './Footer/Footer';
 import Product from './Product/Product';
 import Categories from './Categories/Categories';
-import Intro from './Intro/Intro';
 import Genre from './Genre/Genre';
 
 export {
-  AboutUs,
-  Chef,
   Newsletter,
   Footer,
   Product,
   Categories,
-  Intro,
   Genre,
 };
