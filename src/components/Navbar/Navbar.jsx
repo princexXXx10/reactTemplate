5b204d78af64f4911f5086a8819156d67e2b6830
princexXXx10 <victorsuccess6@gmail.com>
2022-10-15 14:38:19 +0100

    S

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,13 +27,13 @@ const Navbar = () => {
 
 
             <div className='navbar__smallScreen'>
-              <p color='#e92121' fontSize={27} onClick={() => setToggleMenu(true)}><MenuIcon/></p>
+              <p onClick={() => setToggleMenu(true)}><MenuIcon/></p>
               
               {toggleMenu && (
                 <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
                   <div className='navbar__content--main'>
                     <img src={images.Logo} alt="" className='overlay__close overlay__closeLogo' />
-                    <p color='#e92121' fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)}><CloseIcon /></p>
+                    <p className='overlay__close' onClick={() => setToggleMenu(false)}><CloseIcon /></p>
                   </div>
                   <ul className='app__navbar-smallscreen-links'>
                     <li className=''><a href='#home'>Home</a></li>
