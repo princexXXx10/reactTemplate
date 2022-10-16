b5c3e03481ac9d7789d3b1093dc1462b8a231185
princexXXx10 <victorsuccess6@gmail.com>
2022-10-16 20:42:49 +0100

    Finished with everything. Just need to change navigation SVG

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -22,14 +22,14 @@ const Contact = () => (
                 <label htmlFor="name"><p><b>Name:</b></p></label>
                 <input type="text" id="name" required />
 
-                <label htmlFor="name"><p><b>Email:</b></p></label>
+                <label htmlFor="Email"><p><b>Email:</b></p></label>
                 <input type="text" id="email" required />
 
-                <label htmlFor="name"><p><b>Phone Number:</b></p></label>
+                <label htmlFor="Phone Number"><p><b>Phone Number:</b></p></label>
                 <input type="text" id="phone" required />
 
-                <label htmlFor="name"><p><b>Message:</b></p></label>
-                <textarea type="message" rows="6" required ></textarea>
+                <label htmlFor="Message"><p><b>Message:</b></p></label>
+                <textarea type="message" rows="4" required ></textarea>
 
                 <div className='center'>
                   <input type="submit" value="Send" className='submit' />
@@ -41,7 +41,7 @@ const Contact = () => (
           <div className="contact__right">
             <div className="contact__address"><img src="https://img.icons8.com/ios-glyphs/30/000000/place-marker.png" alt='Location icon - (Icon8)' className="icon"/><p>Homer’s Plaza, Opp. Winners’ Chapel, Uromi-ilushi Road, Orhuen-Ubiaja, Edo State.</p></div>
             <div className="contact__phone"><img src="https://img.icons8.com/ios-glyphs/30/000000/phone--v1.png" alt='Phone icon - (Icon8)' className="icon"/><p>+234 9023456789</p></div>
-            <div className="contact__email"><img src="https://img.icons8.com/ios-glyphs/30/000000/phone--v1.png" alt='Email icon - (Icon8)' className="icon"/><p>omoikefarms@gmail.com</p></div>
+            <div className="contact__email"><img src="https://img.icons8.com/material-sharp/24/000000/new-post.png" alt='Email icon - (Icon8)' className="icon"/><p>omoikefarms@gmail.com</p></div>
           </div>
         </div>
 
