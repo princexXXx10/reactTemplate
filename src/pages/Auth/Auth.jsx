a46624ea839de8b9d96cbb8eac7bedef5465f7b0
princexXXx10 <victorsuccess6@gmail>
2026-01-01 12:18:39 +0100

    feat: add ProductPage component for displaying product details and Auth component for user login/registration.

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './Auth.css';
 
 const Auth = () => {
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { games } from '../../container/data';
 import sliderData from '../../components/Slider/sliderData';
 import ProductItem from '../../components/ProductItem/ProductItem'; // Re-use ProductItem for similar products
